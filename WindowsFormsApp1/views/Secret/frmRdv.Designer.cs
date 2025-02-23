269b1082c58e77dee2423bcf5412c9df3caf06f7
Houleymatou DIALLO <dhouleymatou150@gmail.com>
2025-02-23 17:53:32 +0000

    ajout des UI pour rendez vous

--- WindowsFormsApp1/views/Secret/frmRdv.Designer.cs
+++ WindowsFormsApp1/views/Secret/frmRdv.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp1.views.Secret
 {
-    partial class frmAcceuilSecret
+    partial class frmRdv
     {
         /// <summary>
         /// Required designer variable.
@@ -29,17 +29,17 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.btnRechercherAgenda = new System.Windows.Forms.Button();
-            this.btnModifierAgenda = new System.Windows.Forms.Button();
-            this.btnVoirRdv = new System.Windows.Forms.Button();
             this.btnAjouterAgenda = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.txtDateChercher = new System.Windows.Forms.DateTimePicker();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -47,27 +47,34 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(114)))), ((int)(((byte)(144)))));
-            this.panel1.Controls.Add(this.groupBox1);
-            this.panel1.Controls.Add(this.btnRechercherAgenda);
-            this.panel1.Controls.Add(this.btnModifierAgenda);
-            this.panel1.Controls.Add(this.btnVoirRdv);
-            this.panel1.Controls.Add(this.btnAjouterAgenda);
-            this.panel1.Controls.Add(this.dataGridView1);
-            this.panel1.Location = new System.Drawing.Point(1, -2);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(-2, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1946, 1108);
+            this.panel1.Size = new System.Drawing.Size(1924, 1048);
             this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(114)))), ((int)(((byte)(144)))));
+            this.panel2.Controls.Add(this.groupBox1);
+            this.panel2.Controls.Add(this.btnRechercherAgenda);
+            this.panel2.Controls.Add(this.btnAjouterAgenda);
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Location = new System.Drawing.Point(-11, -30);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1946, 1108);
+            this.panel2.TabIndex = 1;
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.txtDateChercher);
+            this.groupBox1.Controls.Add(this.comboBox1);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.textBox2);
-            this.groupBox1.Controls.Add(this.textBox1);
             this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.groupBox1.Location = new System.Drawing.Point(305, 3);
+            this.groupBox1.Location = new System.Drawing.Point(125, 39);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(763, 163);
+            this.groupBox1.Size = new System.Drawing.Size(930, 163);
             this.groupBox1.TabIndex = 15;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Rechercher";
@@ -77,32 +84,18 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(23, 85);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.Size = new System.Drawing.Size(44, 20);
             this.label2.TabIndex = 22;
-            this.label2.Text = "Email";
+            this.label2.Text = "Date";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(23, 31);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(84, 20);
+            this.label1.Size = new System.Drawing.Size(78, 20);
             this.label1.TabIndex = 21;
-            this.label1.Text = "Telephone";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(27, 108);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(703, 26);
-            this.textBox2.TabIndex = 20;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(27, 54);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(703, 26);
-            this.textBox1.TabIndex = 19;
+            this.label1.Text = "Specialite";
             // 
             // btnRechercherAgenda
             // 
@@ -111,41 +104,13 @@
             this.btnRechercherAgenda.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnRechercherAgenda.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnRechercherAgenda.ForeColor = System.Drawing.Color.White;
-            this.btnRechercherAgenda.Location = new System.Drawing.Point(1114, 37);
+            this.btnRechercherAgenda.Location = new System.Drawing.Point(1061, 70);
             this.btnRechercherAgenda.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnRechercherAgenda.Name = "btnRechercherAgenda";
             this.btnRechercherAgenda.Size = new System.Drawing.Size(220, 40);
             this.btnRechercherAgenda.TabIndex = 14;
-            this.btnRechercherAgenda.Text = "&Rechercher un Patient";
+            this.btnRechercherAgenda.Text = "&Voir Disponibilites";
             this.btnRechercherAgenda.UseVisualStyleBackColor = true;
-            // 
-            // btnModifierAgenda
-            // 
-            this.btnModifierAgenda.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnModifierAgenda.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(169)))), ((int)(((byte)(155)))));
-            this.btnModifierAgenda.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnModifierAgenda.ForeColor = System.Drawing.Color.White;
-            this.btnModifierAgenda.Location = new System.Drawing.Point(11, 79);
-            this.btnModifierAgenda.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.btnModifierAgenda.Name = "btnModifierAgenda";
-            this.btnModifierAgenda.Size = new System.Drawing.Size(244, 39);
-            this.btnModifierAgenda.TabIndex = 13;
-            this.btnModifierAgenda.Text = "&Modifier un Agenda";
-            this.btnModifierAgenda.UseVisualStyleBackColor = true;
-            // 
-            // btnVoirRdv
-            // 
-            this.btnVoirRdv.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnVoirRdv.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(169)))), ((int)(((byte)(155)))));
-            this.btnVoirRdv.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnVoirRdv.ForeColor = System.Drawing.Color.White;
-            this.btnVoirRdv.Location = new System.Drawing.Point(11, 135);
-            this.btnVoirRdv.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.btnVoirRdv.Name = "btnVoirRdv";
-            this.btnVoirRdv.Size = new System.Drawing.Size(248, 39);
-            this.btnVoirRdv.TabIndex = 12;
-            this.btnVoirRdv.Text = "&Voir mes rendez-vous";
-            this.btnVoirRdv.UseVisualStyleBackColor = true;
             // 
             // btnAjouterAgenda
             // 
@@ -154,36 +119,52 @@
             this.btnAjouterAgenda.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnAjouterAgenda.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAjouterAgenda.ForeColor = System.Drawing.Color.White;
-            this.btnAjouterAgenda.Location = new System.Drawing.Point(11, 15);
+            this.btnAjouterAgenda.Location = new System.Drawing.Point(1061, 145);
             this.btnAjouterAgenda.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnAjouterAgenda.Name = "btnAjouterAgenda";
-            this.btnAjouterAgenda.Size = new System.Drawing.Size(244, 39);
+            this.btnAjouterAgenda.Size = new System.Drawing.Size(233, 39);
             this.btnAjouterAgenda.TabIndex = 11;
-            this.btnAjouterAgenda.Text = "Ajouter un &Patient";
+            this.btnAjouterAgenda.Text = "Prendre &Rendez-Vous";
             this.btnAjouterAgenda.UseVisualStyleBackColor = true;
             // 
             // dataGridView1
             // 
             this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(3, 193);
+            this.dataGridView1.Location = new System.Drawing.Point(18, 208);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(2164, 1150);
+            this.dataGridView1.Size = new System.Drawing.Size(2056, 968);
             this.dataGridView1.TabIndex = 0;
             // 
-            // frmAcceuilSecret
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(27, 54);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(703, 28);
+            this.comboBox1.TabIndex = 23;
+            // 
+            // txtDateChercher
+            // 
+            this.txtDateChercher.Location = new System.Drawing.Point(27, 119);
+            this.txtDateChercher.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtDateChercher.Name = "txtDateChercher";
+            this.txtDateChercher.Size = new System.Drawing.Size(703, 26);
+            this.txtDateChercher.TabIndex = 24;
+            // 
+            // frmRdv
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1946, 1106);
+            this.ClientSize = new System.Drawing.Size(1924, 1050);
             this.ControlBox = false;
             this.Controls.Add(this.panel1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "frmAcceuilSecret";
-            this.Text = "frmAcceuilSecret";
+            this.Name = "frmRdv";
+            this.Text = "Rendez vous";
             this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -194,15 +175,14 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button btnRechercherAgenda;
-        private System.Windows.Forms.Button btnModifierAgenda;
-        private System.Windows.Forms.Button btnVoirRdv;
-        private System.Windows.Forms.Button btnAjouterAgenda;
+        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnRechercherAgenda;
+        private System.Windows.Forms.Button btnAjouterAgenda;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DateTimePicker txtDateChercher;
     }
 }
---
+++ WindowsFormsApp1/views/Secret/frmValiderRdv.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace WindowsFormsApp1.views.Secret
+{
+    partial class frmValiderRdv
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cbbCreneaux = new System.Windows.Forms.ComboBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Soins = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.entityCommand1 = new System.Data.Entity.Core.EntityClient.EntityCommand();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnValider = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(114)))), ((int)(((byte)(144)))));
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(0, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1929, 1052);
+            this.panel1.TabIndex = 0;
+            // 
+            // cbbCreneaux
+            // 
+            this.cbbCreneaux.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cbbCreneaux.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cbbCreneaux.FormattingEnabled = true;
+            this.cbbCreneaux.Location = new System.Drawing.Point(25, 129);
+            this.cbbCreneaux.Name = "cbbCreneaux";
+            this.cbbCreneaux.Size = new System.Drawing.Size(121, 28);
+            this.cbbCreneaux.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel2.Controls.Add(this.btnValider);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.comboBox3);
+            this.panel2.Controls.Add(this.Soins);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.comboBox2);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.cbbCreneaux);
+            this.panel2.Location = new System.Drawing.Point(419, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(509, 639);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Creneaux";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(165, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(198, 36);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Rendez-Vous";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Mode Paiement";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(25, 230);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 28);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 301);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(155, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Reference Paiement";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 342);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(129, 26);
+            this.textBox1.TabIndex = 8;
+            // 
+            // Soins
+            // 
+            this.Soins.AutoSize = true;
+            this.Soins.Location = new System.Drawing.Point(30, 393);
+            this.Soins.Name = "Soins";
+            this.Soins.Size = new System.Drawing.Size(49, 20);
+            this.Soins.TabIndex = 9;
+            this.Soins.Text = "Soins";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(25, 438);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(254, 28);
+            this.comboBox3.TabIndex = 10;
+            // 
+            // entityCommand1
+            // 
+            this.entityCommand1.CommandTimeout = 0;
+            this.entityCommand1.CommandTree = null;
+            this.entityCommand1.Connection = null;
+            this.entityCommand1.EnablePlanCaching = true;
+            this.entityCommand1.Transaction = null;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(25, 518);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(129, 26);
+            this.textBox2.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 484);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Couts";
+            // 
+            // btnValider
+            // 
+            this.btnValider.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(114)))), ((int)(((byte)(144)))));
+            this.btnValider.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnValider.Location = new System.Drawing.Point(182, 570);
+            this.btnValider.Name = "btnValider";
+            this.btnValider.Size = new System.Drawing.Size(148, 44);
+            this.btnValider.TabIndex = 13;
+            this.btnValider.Text = "Valider";
+            this.btnValider.UseVisualStyleBackColor = false;
+            // 
+            // frmValiderRdv
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1924, 1050);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.Name = "frmValiderRdv";
+            this.Text = "frmValiderRdv";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbbCreneaux;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label Soins;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Data.Entity.Core.EntityClient.EntityCommand entityCommand1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnValider;
+    }
+}
