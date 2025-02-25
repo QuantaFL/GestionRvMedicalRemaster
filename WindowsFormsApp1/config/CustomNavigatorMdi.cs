c52a3bdc72bd935013d60e266cb55b5bd8d8959e
Cheikh Tidiane TRAORÉ <atidiane741@gmail.com>
2025-02-26 00:23:25 +0100

    log the custom navigation method for mdi

--- WindowsFormsApp1/config/CustomNavigatorMdi.cs
+++ WindowsFormsApp1/config/CustomNavigatorMdi.cs
@@ -1,16 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Serilog;
+using System;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1.config
 {
+    /// <summary>
+    /// Une classe utilitaire pour gérer la navigation MDI (ouverture et fermeture de formulaires enfants)
+    /// dans un formulaire parent MDI, incluant la journalisation des actions avec Serilog.
+    /// </summary>
     class CustomNavigatorMdi
     {
+        /// <summary>
+        /// Ferme tous les formulaires enfants actuellement ouverts dans le formulaire parent MDI spécifié.
+        /// Cette fonction est utile pour passer d'une vue à l'autre ou réinitialiser l'interface utilisateur.
+        /// </summary>
+        /// <param name="mdiParent">Le formulaire parent MDI contenant les formulaires enfants à fermer.</param>
         public static void fermer(Form mdiParent)
         {
+            Log.Information("Fermeture de tous les formulaires enfants.");
+
             Form[] charr = mdiParent.MdiChildren;
             foreach (Form f in charr)
             {
@@ -18,6 +26,13 @@ namespace WindowsFormsApp1.config
             }
         }
 
+        /// <summary>
+        /// Ouvre un formulaire enfant dans un parent MDI tout en désactivant un bouton spécifique.
+        /// Ferme tous les autres formulaires enfants ouverts et ajuste l'état de la fenêtre du formulaire enfant.
+        /// </summary>
+        /// <param name="mdiParent">Le formulaire parent MDI dans lequel ouvrir le formulaire enfant.</param>
+        /// <param name="buttonToDisable">Le bouton qui sera désactivé pendant l'ouverture du formulaire enfant.</param>
+        /// <param name="mdiChild">Le formulaire enfant à ouvrir.</param>
         public static void push(Form mdiParent, Button buttonToDisable, Form mdiChild)
         {
             buttonToDisable.Enabled = false;
@@ -25,6 +40,7 @@ namespace WindowsFormsApp1.config
             fermer(mdiParent);
 
             mdiChild.MdiParent = mdiParent;
+
             mdiChild.Show();
             mdiChild.WindowState = FormWindowState.Maximized;
 
@@ -32,7 +48,8 @@ namespace WindowsFormsApp1.config
             {
                 buttonToDisable.Enabled = true;
             };
-        }
 
+            Log.Information("Formulaire enfant '{0}' ouvert dans le parent MDI.", mdiChild.Name);
+        }
     }
 }
