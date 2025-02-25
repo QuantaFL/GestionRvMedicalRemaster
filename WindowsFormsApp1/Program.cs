7df82817bfd42f9829e48501a7b43f4c11f2deb4
Cheikh Tidiane TRAORÉ <atidiane741@gmail.com>
2025-02-25 01:46:57 +0100

    ädd connexion logic

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -40,7 +40,7 @@ namespace WindowsFormsApp1
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new frmDashAdmin());
             //Application.Run(new frmExecutionReussie("good Morning"));
-            Application.Run(new frmAcceuilSecret());
+            Application.Run(new FrmConnexion());
 
             // vider linstance seri/og
             // Log.CloseAndFlush();
