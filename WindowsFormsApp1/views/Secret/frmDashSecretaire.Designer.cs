a5c6e091e8677bad480693ba5292f7184d3f4793
Cheikh Tidiane TRAORÉ <atidiane741@gmail.com>
2025-02-26 17:52:39 +0100

    fiix

--- WindowsFormsApp1/views/Secret/frmDashSecretaire.Designer.cs
+++ WindowsFormsApp1/views/Secret/frmDashSecretaire.Designer.cs
@@ -74,16 +74,18 @@
             this.panel4.Controls.Add(this.panel1);
             this.panel4.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(418, 937);
+            this.panel4.Size = new System.Drawing.Size(279, 487);
             this.panel4.TabIndex = 7;
             // 
             // pictureBox6
             // 
             this.pictureBox6.Image = global::WindowsFormsApp1.Properties.Resources.settings;
-            this.pictureBox6.Location = new System.Drawing.Point(9, 1035);
+            this.pictureBox6.Location = new System.Drawing.Point(6, 673);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(63, 60);
+            this.pictureBox6.Size = new System.Drawing.Size(42, 39);
             this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox6.TabIndex = 13;
             this.pictureBox6.TabStop = false;
@@ -91,9 +93,10 @@
             // pictureBox4
             // 
             this.pictureBox4.Image = global::WindowsFormsApp1.Properties.Resources.users_medical__1_;
-            this.pictureBox4.Location = new System.Drawing.Point(3, 623);
+            this.pictureBox4.Location = new System.Drawing.Point(2, 405);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(70, 60);
+            this.pictureBox4.Size = new System.Drawing.Size(47, 39);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox4.TabIndex = 12;
             this.pictureBox4.TabStop = false;
@@ -104,9 +107,10 @@
             this.btnDeconnexion.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnDeconnexion.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDeconnexion.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnDeconnexion.Location = new System.Drawing.Point(86, 732);
+            this.btnDeconnexion.Location = new System.Drawing.Point(57, 476);
+            this.btnDeconnexion.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnDeconnexion.Name = "btnDeconnexion";
-            this.btnDeconnexion.Size = new System.Drawing.Size(321, 60);
+            this.btnDeconnexion.Size = new System.Drawing.Size(214, 39);
             this.btnDeconnexion.TabIndex = 11;
             this.btnDeconnexion.Text = "&Deconnexion";
             this.btnDeconnexion.UseVisualStyleBackColor = false;
@@ -115,9 +119,10 @@
             // pictureBox3
             // 
             this.pictureBox3.Image = global::WindowsFormsApp1.Properties.Resources.user_doctor;
-            this.pictureBox3.Location = new System.Drawing.Point(6, 732);
+            this.pictureBox3.Location = new System.Drawing.Point(4, 476);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(70, 60);
+            this.pictureBox3.Size = new System.Drawing.Size(47, 39);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox3.TabIndex = 10;
             this.pictureBox3.TabStop = false;
@@ -128,9 +133,10 @@
             this.btnPatient.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnPatient.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnPatient.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnPatient.Location = new System.Drawing.Point(81, 623);
+            this.btnPatient.Location = new System.Drawing.Point(54, 405);
+            this.btnPatient.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnPatient.Name = "btnPatient";
-            this.btnPatient.Size = new System.Drawing.Size(327, 60);
+            this.btnPatient.Size = new System.Drawing.Size(218, 39);
             this.btnPatient.TabIndex = 9;
             this.btnPatient.Text = "&PATIENTS";
             this.btnPatient.UseVisualStyleBackColor = false;
@@ -139,9 +145,10 @@
             // pictureBox5
             // 
             this.pictureBox5.Image = global::WindowsFormsApp1.Properties.Resources.settings;
-            this.pictureBox5.Location = new System.Drawing.Point(6, 512);
+            this.pictureBox5.Location = new System.Drawing.Point(4, 333);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(63, 60);
+            this.pictureBox5.Size = new System.Drawing.Size(42, 39);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox5.TabIndex = 8;
             this.pictureBox5.TabStop = false;
@@ -150,9 +157,10 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = global::WindowsFormsApp1.Properties.Resources.calendar_day;
-            this.pictureBox2.Location = new System.Drawing.Point(3, 292);
+            this.pictureBox2.Location = new System.Drawing.Point(2, 190);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(70, 60);
+            this.pictureBox2.Size = new System.Drawing.Size(47, 39);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 7;
             this.pictureBox2.TabStop = false;
@@ -160,9 +168,10 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.booking__1_;
-            this.pictureBox1.Location = new System.Drawing.Point(-3, 403);
+            this.pictureBox1.Location = new System.Drawing.Point(-2, 262);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(84, 60);
+            this.pictureBox1.Size = new System.Drawing.Size(56, 39);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
@@ -172,9 +181,10 @@
             this.btnParametre.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnParametre.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnParametre.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnParametre.Location = new System.Drawing.Point(76, 512);
+            this.btnParametre.Location = new System.Drawing.Point(51, 333);
+            this.btnParametre.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnParametre.Name = "btnParametre";
-            this.btnParametre.Size = new System.Drawing.Size(322, 60);
+            this.btnParametre.Size = new System.Drawing.Size(215, 39);
             this.btnParametre.TabIndex = 7;
             this.btnParametre.Text = "RENDEZ-VOUS";
             this.btnParametre.UseVisualStyleBackColor = false;
@@ -185,9 +195,10 @@
             this.btnListeUtilisateur.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnListeUtilisateur.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnListeUtilisateur.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnListeUtilisateur.Location = new System.Drawing.Point(81, 292);
+            this.btnListeUtilisateur.Location = new System.Drawing.Point(54, 190);
+            this.btnListeUtilisateur.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnListeUtilisateur.Name = "btnListeUtilisateur";
-            this.btnListeUtilisateur.Size = new System.Drawing.Size(327, 60);
+            this.btnListeUtilisateur.Size = new System.Drawing.Size(218, 39);
             this.btnListeUtilisateur.TabIndex = 5;
             this.btnListeUtilisateur.Text = "&GERER RDV";
             this.btnListeUtilisateur.UseVisualStyleBackColor = false;
@@ -198,9 +209,10 @@
             this.btnCorbeille.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnCorbeille.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCorbeille.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnCorbeille.Location = new System.Drawing.Point(81, 403);
+            this.btnCorbeille.Location = new System.Drawing.Point(54, 262);
+            this.btnCorbeille.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCorbeille.Name = "btnCorbeille";
-            this.btnCorbeille.Size = new System.Drawing.Size(322, 60);
+            this.btnCorbeille.Size = new System.Drawing.Size(215, 39);
             this.btnCorbeille.TabIndex = 4;
             this.btnCorbeille.Text = "&AGENDA";
             this.btnCorbeille.UseVisualStyleBackColor = false;
@@ -212,9 +224,10 @@
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.pictureBox7);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(12, 25);
+            this.panel1.Location = new System.Drawing.Point(8, 16);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(404, 192);
+            this.panel1.Size = new System.Drawing.Size(269, 125);
             this.panel1.TabIndex = 0;
             // 
             // lblName
@@ -222,9 +235,10 @@
             this.lblName.AutoSize = true;
             this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblName.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.lblName.Location = new System.Drawing.Point(192, 163);
+            this.lblName.Location = new System.Drawing.Point(128, 106);
+            this.lblName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(66, 29);
+            this.lblName.Size = new System.Drawing.Size(44, 20);
             this.lblName.TabIndex = 11;
             this.lblName.Text = "Ouly";
             // 
@@ -233,18 +247,20 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label2.Location = new System.Drawing.Point(3, 162);
+            this.label2.Location = new System.Drawing.Point(2, 105);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(165, 32);
+            this.label2.Size = new System.Drawing.Size(111, 24);
             this.label2.TabIndex = 10;
             this.label2.Text = "Bienvenue :";
             // 
             // pictureBox7
             // 
             this.pictureBox7.Image = global::WindowsFormsApp1.Properties.Resources.lgcr;
-            this.pictureBox7.Location = new System.Drawing.Point(-3, 52);
+            this.pictureBox7.Location = new System.Drawing.Point(-2, 34);
+            this.pictureBox7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(102, 77);
+            this.pictureBox7.Size = new System.Drawing.Size(68, 50);
             this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox7.TabIndex = 9;
             this.pictureBox7.TabStop = false;
@@ -254,23 +270,24 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label1.Location = new System.Drawing.Point(120, 65);
+            this.label1.Location = new System.Drawing.Point(80, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(281, 55);
+            this.label1.Size = new System.Drawing.Size(187, 37);
             this.label1.TabIndex = 6;
             this.label1.Text = "Health Care";
             // 
             // frmDashSecretaire
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1461, 937);
+            this.ClientSize = new System.Drawing.Size(913, 487);
             this.ControlBox = false;
             this.Controls.Add(this.panel4);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.IsMdiContainer = true;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmDashSecretaire";
-            this.Text = "Bienvenue";
             this.Load += new System.EventHandler(this.frmDashSecretaire_Load);
             this.panel4.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
