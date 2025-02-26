6397916c4dd9a521365bc05629797c14d93fa8c0
Cheikh Tidiane TRAORÉ <atidiane741@gmail.com>
2025-02-26 17:36:04 +0100

    fiix

--- WindowsFormsApp1/views/Secret/frmListerRdv.Designer.cs
+++ WindowsFormsApp1/views/Secret/frmListerRdv.Designer.cs
@@ -40,18 +40,20 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(378, 32);
+            this.label1.Location = new System.Drawing.Point(252, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(527, 46);
+            this.label1.Size = new System.Drawing.Size(359, 31);
             this.label1.TabIndex = 6;
             this.label1.Text = "LISTE DES RENDEZ-VOUS";
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = global::WindowsFormsApp1.Properties.Resources.rdv;
-            this.pictureBox2.Location = new System.Drawing.Point(714, 118);
+            this.pictureBox2.Location = new System.Drawing.Point(476, 77);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(1372, 620);
+            this.pictureBox2.Size = new System.Drawing.Size(915, 403);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 8;
             this.pictureBox2.TabStop = false;
@@ -61,25 +63,26 @@
             this.dgRdv.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(214)))), ((int)(((byte)(220)))), ((int)(((byte)(234)))));
             this.dgRdv.CausesValidation = false;
             this.dgRdv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgRdv.Location = new System.Drawing.Point(1, 118);
+            this.dgRdv.Location = new System.Drawing.Point(1, 77);
+            this.dgRdv.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dgRdv.Name = "dgRdv";
             this.dgRdv.RowHeadersWidth = 62;
             this.dgRdv.RowTemplate.Height = 28;
-            this.dgRdv.Size = new System.Drawing.Size(713, 662);
+            this.dgRdv.Size = new System.Drawing.Size(475, 430);
             this.dgRdv.TabIndex = 7;
             // 
             // frmListerRdv
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(114)))), ((int)(((byte)(144)))));
-            this.ClientSize = new System.Drawing.Size(1374, 737);
+            this.ClientSize = new System.Drawing.Size(913, 479);
             this.ControlBox = false;
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.dgRdv);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmListerRdv";
-            this.Text = "frmListerRdv";
             this.Load += new System.EventHandler(this.frmListerRdv_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgRdv)).EndInit();
