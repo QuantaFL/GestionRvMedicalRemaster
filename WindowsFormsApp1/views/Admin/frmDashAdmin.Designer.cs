c1cef128a2f71233bd59092f5082bc2dbe53923e
RBen19 <bonmerciel@gmail.com>
2025-02-26 14:03:43 +0100

    modification de l'ui de admin

--- WindowsFormsApp1/views/Admin/frmDashAdmin.Designer.cs
+++ WindowsFormsApp1/views/Admin/frmDashAdmin.Designer.cs
@@ -30,29 +30,26 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmDashAdmin));
             this.panel4 = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.lblNomUtilisateur = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.btnListeUtilisateur = new System.Windows.Forms.Button();
-            this.btnCorbeille = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnListeUtilisateur = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel4.SuspendLayout();
-            this.panel2.SuspendLayout();
-            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             this.SuspendLayout();
             // 
@@ -64,9 +61,7 @@
             this.panel4.Controls.Add(this.panel2);
             this.panel4.Controls.Add(this.pictureBox5);
             this.panel4.Controls.Add(this.pictureBox2);
-            this.panel4.Controls.Add(this.pictureBox1);
             this.panel4.Controls.Add(this.btnListeUtilisateur);
-            this.panel4.Controls.Add(this.btnCorbeille);
             this.panel4.Controls.Add(this.panel1);
             this.panel4.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel4.Location = new System.Drawing.Point(0, 0);
@@ -75,12 +70,23 @@
             this.panel4.Size = new System.Drawing.Size(364, 599);
             this.panel4.TabIndex = 6;
             // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::WindowsFormsApp1.Properties.Resources.poubelle_de_recyclage;
+            this.pictureBox3.Location = new System.Drawing.Point(2, 424);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(75, 48);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
             // button1
             // 
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button1.Location = new System.Drawing.Point(75, 485);
+            this.button1.Location = new System.Drawing.Point(77, 424);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(287, 47);
@@ -122,77 +128,6 @@
             this.label2.TabIndex = 10;
             this.label2.Text = "Bienvenue :";
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label4.Location = new System.Drawing.Point(105, 52);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(232, 46);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Health Care";
-            // 
-            // btnListeUtilisateur
-            // 
-            this.btnListeUtilisateur.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnListeUtilisateur.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnListeUtilisateur.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnListeUtilisateur.Location = new System.Drawing.Point(71, 319);
-            this.btnListeUtilisateur.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
-            this.btnListeUtilisateur.Name = "btnListeUtilisateur";
-            this.btnListeUtilisateur.Size = new System.Drawing.Size(289, 48);
-            this.btnListeUtilisateur.TabIndex = 5;
-            this.btnListeUtilisateur.Text = "Afficher les &Utilisateurs";
-            this.btnListeUtilisateur.UseVisualStyleBackColor = false;
-            this.btnListeUtilisateur.Click += new System.EventHandler(this.btnListeUtilisateur_Click);
-            // 
-            // btnCorbeille
-            // 
-            this.btnCorbeille.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnCorbeille.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCorbeille.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnCorbeille.Location = new System.Drawing.Point(73, 399);
-            this.btnCorbeille.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
-            this.btnCorbeille.Name = "btnCorbeille";
-            this.btnCorbeille.Size = new System.Drawing.Size(287, 47);
-            this.btnCorbeille.TabIndex = 4;
-            this.btnCorbeille.Text = "&Corbeille";
-            this.btnCorbeille.UseVisualStyleBackColor = false;
-            this.btnCorbeille.Click += new System.EventHandler(this.btnCorbeille_Click);
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.pictureBox6);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(361, 71);
-            this.panel1.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label1.Location = new System.Drawing.Point(43, 15);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(102, 39);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Menu";
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Image = global::WindowsFormsApp1.Properties.Resources.poubelle_de_recyclage;
-            this.pictureBox3.Location = new System.Drawing.Point(0, 485);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(75, 48);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox3.TabIndex = 11;
-            this.pictureBox3.TabStop = false;
-            // 
             // pictureBox7
             // 
             this.pictureBox7.Image = global::WindowsFormsApp1.Properties.Resources.lgcr;
@@ -203,6 +138,17 @@
             this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox7.TabIndex = 9;
             this.pictureBox7.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(105, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(232, 46);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Health Care";
             // 
             // pictureBox5
             // 
@@ -226,16 +172,29 @@
             this.pictureBox2.TabIndex = 7;
             this.pictureBox2.TabStop = false;
             // 
-            // pictureBox1
+            // btnListeUtilisateur
             // 
-            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.poubelle_de_recyclage;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 396);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(75, 48);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 6;
-            this.pictureBox1.TabStop = false;
+            this.btnListeUtilisateur.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnListeUtilisateur.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnListeUtilisateur.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnListeUtilisateur.Location = new System.Drawing.Point(71, 319);
+            this.btnListeUtilisateur.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
+            this.btnListeUtilisateur.Name = "btnListeUtilisateur";
+            this.btnListeUtilisateur.Size = new System.Drawing.Size(289, 48);
+            this.btnListeUtilisateur.TabIndex = 5;
+            this.btnListeUtilisateur.Text = "Afficher les &Utilisateurs";
+            this.btnListeUtilisateur.UseVisualStyleBackColor = false;
+            this.btnListeUtilisateur.Click += new System.EventHandler(this.btnListeUtilisateur_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox6);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(361, 71);
+            this.panel1.TabIndex = 0;
             // 
             // pictureBox6
             // 
@@ -247,6 +206,17 @@
             this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox6.TabIndex = 8;
             this.pictureBox6.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(43, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 39);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Menu";
             // 
             // frmDashAdmin
             // 
@@ -263,15 +233,14 @@
             this.Text = "Tableau de bord";
             this.Load += new System.EventHandler(this.frmDashAdmin_Load);
             this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             this.ResumeLayout(false);
 
@@ -282,9 +251,7 @@
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.PictureBox pictureBox5;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button btnListeUtilisateur;
-        private System.Windows.Forms.Button btnCorbeille;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox pictureBox6;
         private System.Windows.Forms.Label label1;
