77a9cd08347bd706b4a96c017b50519538e1138d
RBen19 <bonmerciel@gmail.com>
2025-05-20 15:26:19 +0200

    feat: create Medecin Entity

---
+++ MetierRvMedical/Models/Medecin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MetierRvMedical.Models
+{
+    public class Medecin : Utilisateur
+    {
+
+        public int? IdSpecialite { get; set; }
+        [ForeignKey("IdSpecialite")]
+
+        public virtual Specialite Specialite { get; set; }
+
+        [MaxLength(10)]
+        public string NumeroOrdre { get; set; }
+        public virtual ICollection<Agenda> agenda { get; set; }
+    }
+}
