49ac45a26e7565c4bdbbb5ce85c7578d382018f6
RBen19 <bonmerciel@gmail.com>
2025-05-20 15:32:51 +0200

    feat: create Secretaire Entity

---
+++ MetierRvMedical/Models/Secretaire.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MetierRvMedical.Models
+{
+    public class Secretaire : Utilisateur
+    {
+        [MaxLength(15)]
+        public string TelephoneFixe { get; set; }
+        [MaxLength(30)]
+        public string Matricule { get; set; }
+    }
+}
