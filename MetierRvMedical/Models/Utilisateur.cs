ec5779fff533054199cdb5267526bbe3c2b5a319
RBen19 <bonmerciel@gmail.com>
2025-05-20 15:01:34 +0200

    feat:  create Utilisateur Entity

---
+++ MetierRvMedical/Models/Utilisateur.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MetierRvMedical.Models
+{
+    public class Utilisateur : Personne
+    {
+        [MaxLength(100)]
+        public string Identifiant { get; set; }
+
+        [MaxLength(250)]
+        public string MotDePasse { get; set; }
+        public bool Status { get; set; }
+        public int IdRole { get; set; }
+        [ForeignKey("IdRole")]
+        public virtual Role Role { get; set; }
+        public int PremiereConnexion { get; set; }
+    }
+}
