218714be44a81beede87bcd1604ffc183d811915
RBen19 <bonmerciel@gmail.com>
2025-05-20 15:25:39 +0200

    feat: create GroupeSanguin Entity

---
+++ MetierRvMedical/Models/GroupeSanguin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MetierRvMedical.Models
+{
+    public class GroupeSanguin
+    {
+        [Key]
+        public int IdGroupeSanguin { get; set; }
+        [Required, MaxLength(3)]
+        public string CodeGroupeSanguin { get; set; }
+    }
+}
