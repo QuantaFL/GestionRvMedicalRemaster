decfe435b4085428e90933c856236da631959501
RBen19 <bonmerciel@gmail.com>
2025-05-20 15:27:50 +0200

    feat: create RendezVous Entity

---
+++ MetierRvMedical/Models/RendezVous.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MetierRvMedical.Models
+{
+    public class RendezVous
+    {
+        [Key]
+        public int IdRendezVous { get; set; }
+        [Required]
+        public String DateRv { get; set; }
+        [Required, Column]
+        public String HeureRv { get; set; }
+        public int? IdSoin { get; set; }
+        [ForeignKey("IdSoin")]
+        public virtual Soin Soin { get; set; }
+
+        public int? IdPatient { get; set; }
+        [ForeignKey("IdPatient")]
+        public virtual Patient Patient { get; set; }
+
+        public int? IdMedecin { get; set; }
+        [ForeignKey("IdMedecin")]
+        public virtual Medecin Medecin { get; set; }
+        public int? IdAgenda { get; set; }
+        [ForeignKey("IdAgenda")]
+        public virtual Agenda Agenda { get; set; }
+        public string CodeRdv { get; set; }
+    }
+}
