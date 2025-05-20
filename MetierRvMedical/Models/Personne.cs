7c8fe2ef1e52e56d61389eaa8fd6f37c8b2132f0
RBen19 <bonmerciel@gmail.com>
2025-05-20 14:59:40 +0200

    feat: create Personne Entity

---
+++ MetierRvMedical/Models/Personne.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MetierRvMedical.Models
+{
+    public class Personne
+    {
+        [Key]
+        public int IdP { get; set; }
+        [Required, MaxLength(100)]
+        public string NomPrenom { get; set; }
+        [Required, MaxLength(100)]
+        public string Addresse { get; set; }
+        [Required, MaxLength(100), DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required, MaxLength(100)]
+        public string Tel { get; set; }
+        [Required]
+        public DateTime DateNaissance { get; set; }
+
+    }
+}
