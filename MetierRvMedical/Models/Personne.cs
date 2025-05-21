b3dc189abe742e2ddac3820a26ac3aa1900f237e
RBen19 <bonmerciel@gmail.com>
2025-05-21 20:29:18 +0200

    feat:  add annotation for serialisation and deserialisation

--- MetierRvMedical/Models/Personne.cs
+++ MetierRvMedical/Models/Personne.cs
@@ -2,24 +2,34 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Web;
 
 namespace MetierRvMedical.Models
 {
+    [DataContract]
+    [KnownType(typeof(Admin))]
+    [KnownType(typeof(Utilisateur))]
+    [KnownType(typeof(Medecin))]
     public class Personne
     {
         [Key]
+        [DataMember]
         public int IdP { get; set; }
         [Required, MaxLength(100)]
+        [DataMember]
         public string NomPrenom { get; set; }
         [Required, MaxLength(100)]
+        [DataMember]
         public string Addresse { get; set; }
         [Required, MaxLength(100), DataType(DataType.EmailAddress)]
+        [DataMember]
         public string Email { get; set; }
         [Required, MaxLength(100)]
+        [DataMember]
         public string Tel { get; set; }
         [Required]
+        [DataMember]
         public DateTime DateNaissance { get; set; }
-
     }
 }
--- MetierRvMedical/Models/Utilisateur.cs
+++ MetierRvMedical/Models/Utilisateur.cs
@@ -4,20 +4,34 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Runtime.Serialization;
 
 namespace MetierRvMedical.Models
 {
+    [DataContract]
+    [KnownType(typeof(Admin))]
+    [KnownType(typeof(Medecin))]
     public class Utilisateur : Personne
     {
+
+        [DataMember]
         [MaxLength(100)]
         public string Identifiant { get; set; }
 
+        [DataMember]
         [MaxLength(250)]
         public string MotDePasse { get; set; }
+
+        [DataMember]
         public bool Status { get; set; }
+
+        [DataMember]
         public int IdRole { get; set; }
-        [ForeignKey("IdRole")]
+
+        [DataMember]
         public virtual Role Role { get; set; }
+
+        [DataMember]
         public int PremiereConnexion { get; set; }
     }
 }
