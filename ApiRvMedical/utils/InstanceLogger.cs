081323c9287816ea99d8d7cedc1d05b5d380266c
RBen19 <bonmerciel@gmail.com>
2025-05-25 22:32:01 +0000

    feat: create utils component

---
+++ ApiRvMedical/utils/InstanceLogger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Serilog;
+using Serilog.Core;
+using Serilog.Formatting.Json;
+
+namespace ApiRvMedical.utils
+{
+    public static class InstanceLogger
+    {
+        public static ILogger  GetInstance()
+        {
+           return Log.Logger = new LoggerConfiguration()
+               .WriteTo.File(new JsonFormatter(), @"C:/Logs/app.log.ApiRvMedical", rollingInterval: RollingInterval.Day)
+               // You can also uncomment Elasticsearch integration here if needed
+               // .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(elasticSearchUrl))
+               // {
+               //     AutoRegisterTemplate = true,
+               //     IndexFormat = "log-{0:yyyy.MM.dd}",
+               // })
+               .CreateLogger();
+        }
+    }
+}
