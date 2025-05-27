6e1cf57484d20f735c5601bd1f339e6a965f9f3c
diallohouleymatou <dhouleymatou150@gmail.com>
2025-05-27 19:18:12 +0100

    ajout des annotationspor la classe personne mere et creation des dossiers interfaces et services sans implementation

--- MetierRvMedical2/Models/Personne.cs
+++ MetierRvMedical2/Models/Personne.cs
@@ -2,25 +2,38 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MetierRvMedical2.Models
 {
+    [DataContract]
     public class Personne
     {
         [Key]
-        public int IdP {  get; set; }
-        [Required,MaxLength(100)]
-        public string NomPrenom {  get; set; }
+        [DataMember]
+        public int IdP { get; set; }
+
         [Required, MaxLength(100)]
+        [DataMember]
+        public string NomPrenom { get; set; }
+
+        [Required, MaxLength(100)]
+        [DataMember]
         public string Addresse { get; set; }
-        [Required, MaxLength(100),DataType(DataType.EmailAddress)]
+
+        [Required, MaxLength(100), DataType(DataType.EmailAddress)]
+        [DataMember]
         public string Email { get; set; }
+
         [Required, MaxLength(100)]
+        [DataMember]
         public string Tel { get; set; }
+
         [Required]
-       public  DateTime DateNaissance { get; set; }
+        [DataMember]
+        public DateTime DateNaissance { get; set; }
 
     }
 }
