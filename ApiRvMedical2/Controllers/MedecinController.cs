d1575ffaa2083ca95033c12070e53893aa5675fc
RBen19 <bonmerciel@gmail.com>
2025-05-28 15:19:30 +0000

    feat: add rolling file

--- ApiRvMedical2/Controllers/MedecinController.cs
+++ ApiRvMedical2/Controllers/MedecinController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
+using ApiRvMedical2.config;
 using ApiRvMedical2.Models;
 using ApiRvMedical2.services;
 using Serilog;
@@ -30,23 +31,23 @@ namespace ApiRvMedical2.Controllers
         /// <returns>Un objet Medecin ou 404 si introuvable</returns>
         public async Task<IHttpActionResult> GetMedecinByNumeroOrdre(string numeroOrdre)
         {
-            Log.Information("GET api/v1/medecin/{NumeroOrdre} called with numeroOrdre={NumeroOrdre}", numeroOrdre);
+           InstanceLogger.GetInstance().Information("GET api/v1/medecin/{NumeroOrdre} called with numeroOrdre={NumeroOrdre}", numeroOrdre);
             try
             {
                 var medecin = await _medService.GetMedecinByNumerOrdre(numeroOrdre);
 
                 if (medecin == null)
                 {
-                    Log.Warning("Medecin not found for numeroOrdre={NumeroOrdre}", numeroOrdre);
+                    InstanceLogger.GetInstance().Warning("Medecin not found for numeroOrdre={NumeroOrdre}", numeroOrdre);
                     return NotFound();
                 }
 
-                Log.Information("Medecin found for numeroOrdre={NumeroOrdre}", numeroOrdre);
+                InstanceLogger.GetInstance().Information("Medecin found for numeroOrdre={NumeroOrdre}", numeroOrdre);
                 return Ok(medecin);
             }
             catch (Exception ex)
             {
-                Log.Error(ex, "Exception in GetMedecinByNumeroOrdre for numeroOrdre={NumeroOrdre}", numeroOrdre);
+                InstanceLogger.GetInstance().Error(ex, "Exception in GetMedecinByNumeroOrdre for numeroOrdre={NumeroOrdre}", numeroOrdre);
                 throw;
             }
         }
@@ -55,7 +56,7 @@ namespace ApiRvMedical2.Controllers
         [Route("api/v1/medecin/bloquer/{numeroOrdre}")]
         public async Task<IHttpActionResult> BloquerMedecin(string numeroOrdre)
         {
-            Log.Information("GET api/v1/medecin/bloquer/{NumeroOrdre} called with numeroOrdre={NumeroOrdre}", numeroOrdre);
+            InstanceLogger.GetInstance().Information("GET api/v1/medecin/bloquer/{NumeroOrdre} called with numeroOrdre={NumeroOrdre}", numeroOrdre);
             try
             {
                 var medecin = await _medService.BloquerMedecin(numeroOrdre);
@@ -64,7 +65,7 @@ namespace ApiRvMedical2.Controllers
                     Log.Warning("BloquerMedecin: Medecin not found for numeroOrdre={NumeroOrdre}", numeroOrdre);
                     return NotFound();
                 }
-                Log.Information("Medecin blocked successfully for numeroOrdre={NumeroOrdre}", numeroOrdre);
+                InstanceLogger.GetInstance().Information("Medecin blocked successfully for numeroOrdre={NumeroOrdre}", numeroOrdre);
                 return Ok(new
                 {
                     message = "Médecin bloqué avec succès.",
@@ -73,7 +74,7 @@ namespace ApiRvMedical2.Controllers
             }
             catch (Exception ex)
             {
-                Log.Error(ex, "Exception in BloquerMedecin for numeroOrdre={NumeroOrdre}", numeroOrdre);
+                InstanceLogger.GetInstance().Error(ex, "Exception in BloquerMedecin for numeroOrdre={NumeroOrdre}", numeroOrdre);
                 throw;
             }
         }
@@ -88,10 +89,10 @@ namespace ApiRvMedical2.Controllers
                 var medecin = await _medService.DebloquerMedecin(numeroOrdre);
                 if (medecin == false)
                 {
-                    Log.Warning("DebloquerMedecin: Medecin not found for numeroOrdre={NumeroOrdre}", numeroOrdre);
+                    InstanceLogger.GetInstance().Warning("DebloquerMedecin: Medecin not found for numeroOrdre={NumeroOrdre}", numeroOrdre);
                     return NotFound();
                 }
-                Log.Information("Medecin unblocked successfully for numeroOrdre={NumeroOrdre}", numeroOrdre);
+                InstanceLogger.GetInstance().Information("Medecin unblocked successfully for numeroOrdre={NumeroOrdre}", numeroOrdre);
                 return Ok(new
                 {
                     message = "Médecin debloqué avec succès.",
@@ -100,7 +101,7 @@ namespace ApiRvMedical2.Controllers
             }
             catch (Exception ex)
             {
-                Log.Error(ex, "Exception in DebloquerMedecin for numeroOrdre={NumeroOrdre}", numeroOrdre);
+                InstanceLogger.GetInstance().Error(ex, "Exception in DebloquerMedecin for numeroOrdre={NumeroOrdre}", numeroOrdre);
                 throw;
             }
         }
@@ -108,33 +109,33 @@ namespace ApiRvMedical2.Controllers
         // GET api/<controller>
         public IEnumerable<string> Get()
         {
-            Log.Information("GET api/medecin called");
+            InstanceLogger.GetInstance().Information("GET api/medecin called");
             return new string[] { "value1", "value2" };
         }
 
         // GET api/<controller>/5
         public string Get(int id)
         {
-            Log.Information("GET api/medecin/{Id} called with id={Id}", id);
+            InstanceLogger.GetInstance().Information("GET api/medecin/{Id} called with id={Id}", id);
             return "value";
         }
 
         // POST api/<controller>
         public void Post([FromBody] string value)
         {
-            Log.Information("POST api/medecin called with value={Value}", value);
+            InstanceLogger.GetInstance().Information("POST api/medecin called with value={Value}", value);
         }
 
         // PUT api/<controller>/5
         public void Put(int id, [FromBody] string value)
         {
-            Log.Information("PUT api/medecin/{Id} called with id={Id}, value={Value}", id, value);
+            InstanceLogger.GetInstance().Information("PUT api/medecin/{Id} called with id={Id}, value={Value}", id, value);
         }
 
         // DELETE api/<controller>/5
         public void Delete(int id)
         {
-            Log.Information("DELETE api/medecin/{Id} called with id={Id}", id);
+            InstanceLogger.GetInstance().Information("DELETE api/medecin/{Id} called with id={Id}", id);
         }
     }
 }
