f08bf0d030ad931116643335f70e7a056cf5fbc6
Cheikh Tidiane TRAORÉ <cheikh.ahmed.tijani@teksi.ca>
2025-05-29 20:29:13 +0000

    Feat: Add IMoyenPaiementService interface and ServiceHost

---
+++ MetierRvMedical2/Services/IMoyenPaiementService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using MetierRvMedical2.Models;
+
+namespace MetierRvMedical2.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IMoyenPaiement" in both code and config file together.
+    [ServiceContract]
+    public interface IMoyenPaiementService
+    {
+        [OperationContract]
+        void DoWork();
+        [OperationContract]
+        List<MoyenDePaiement> GetAllMoyenDePaiements();
+    }
+}
