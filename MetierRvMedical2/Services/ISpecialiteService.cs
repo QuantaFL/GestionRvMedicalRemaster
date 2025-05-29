eb8637bcf85acf29ae539c394634933dd1842013
Cheikh Tidiane TRAORÉ <cheikh.ahmed.tijani@teksi.ca>
2025-05-29 20:51:20 +0000

    Feat: Add WCF service for managing specialties

---
+++ MetierRvMedical2/Services/ISpecialiteService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using MetierRvMedical2.Models;
+
+namespace MetierRvMedical2.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ISpecialiteService" in both code and config file together.
+    [ServiceContract]
+    public interface ISpecialiteService
+    {
+        [OperationContract]
+        void DoWork();
+        [OperationContract]
+        List<Specialite> GetAllSpecialites();
+        [OperationContract]
+        Specialite GetSpecialiteByCode(String code);
+    }
+}
---
+++ MetierRvMedical2/Services/SpecialiteService.svc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using MetierRvMedical2.Models;
+
+namespace MetierRvMedical2.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "SpecialiteService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select SpecialiteService.svc or SpecialiteService.svc.cs at the Solution Explorer and start debugging.
+    public class SpecialiteService : ISpecialiteService
+    {
+        public void DoWork()
+        {
+        }
+        private bdRdvMedicalContext _db;
+
+        public SpecialiteService(bdRdvMedicalContext context)
+        {
+            _db = context;
+        }
+        public SpecialiteService()
+        {
+            _db = new bdRdvMedicalContext();
+        }
+
+        public List<Specialite> GetAllSpecialites()
+        {
+            return _db.Specialite.ToList();
+        }
+
+        public Specialite GetSpecialiteByCode(string code)
+        {
+            return _db.Specialite.FirstOrDefault(s => s.CodeSpecialte == code);
+        }
+    }
+}
