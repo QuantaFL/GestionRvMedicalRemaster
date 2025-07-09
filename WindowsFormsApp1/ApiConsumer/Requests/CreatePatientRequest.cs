0836f2574442f464265d2e4ae7333ee0e668e368
RBen19 <bonmerciel@gmail.com>
2025-07-09 08:40:15 +0000

    fix: fix make correspond request with api responses

--- WindowsFormsApp1/ApiConsumer/Requests/CreatePatientRequest.cs
+++ WindowsFormsApp1/ApiConsumer/Requests/CreatePatientRequest.cs
@@ -14,16 +14,29 @@ namespace WindowsFormsApp1.ApiConsumer.Requests
         [JsonPropertyName("date_naissance")]
         public DateTime DateNaissance { get; set; }
 
-        [JsonProperty("genre")]
-        [JsonPropertyName("genre")]
-        public string Genre { get; set; }
+        /*
 
-        [JsonProperty("adresse")]
-        [JsonPropertyName("adresse")]
+                   [JsonProperty("genre")]
+         [JsonPropertyName("genre")]
+         public string Genre { get; set; }
+         */
+
+
+        [JsonProperty("taille")]
+        [JsonPropertyName("taille")]
+        public float? taille { get; set; }
+
+
+        [JsonProperty("poids")]
+        [JsonPropertyName("poids")]
+        public float? poids { get; set; }
+
+        [JsonProperty("addresse")]
+        [JsonPropertyName("addresse")]
         public string Adresse { get; set; }
 
-        [JsonProperty("telephone")]
-        [JsonPropertyName("telephone")]
+        [JsonProperty("tel")]
+        [JsonPropertyName("tel")]
         public string Telephone { get; set; }
 
         [JsonProperty("email")]
