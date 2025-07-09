9458df8a30d949522a1cb2fbcbbcb3c703b9cd38
RBen19 <bonmerciel@gmail.com>
2025-07-09 09:59:54 +0000

    feat: create listViewModel on api model

---
+++ WindowsFormsApp1/ApiConsumer/Models/SelectListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.ApiConsumer.Models
+{
+    public class SelectListViewModel
+    {
+       
+            public string Value { get; set; }
+            public string Text { get; set; }
+       
+    }
+}
